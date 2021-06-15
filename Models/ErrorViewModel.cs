16749cfb29b096cb3a5737e11f2ff2cfd6e85c1c
Orbey Paltacı <orbeypaltaci@gmail.com>
2021-06-15 15:20:15 +0300

    Initial Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace UserDirectory.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
